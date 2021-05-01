ff7afe590d57370ae9ddc1695044a78951bc0e58
Alexandre Oliveira <alexandrevoliveira30@gmail.com>
2021-05-01 18:55:55 -0300

    add: Arquivos de rotas de candidatos e empresas | edit: index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const routes = express.Router()
 
+const companies = require('./companies')
+const candidates = require('./candidates')
+
+routes.use("/company", companies)
+routes.use("/candidate", candidates)
+
 // Alias
 routes.get("/", (req, res) => {
-    return res.status(200).send({
-        message: "Welcome",
+    return res.status(200).json({
+        message: "Welcome"
     })
 })
 
